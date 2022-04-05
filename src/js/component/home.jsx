266e75246fa63c298b63bd7f1de8e2f890fba40d
aemem <anamoliner.marques@gmail.com>
2022-04-05 18:41:31 +0000

    primero

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
+	const [input, setInput] = useState=("");
+	const [list, setList] = useState([]);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container ">
+			<div className="mx-auto mb-3 w-25">
+				<label htmlFor="exampleInputEmail1">
+					<h2>To Do</h2>
+				</label>
+				<div className="d-flex">
+					<input
+						type="text"
+						className="form-control"
+						id="inputTask"
+						aria-describedby="emailHelp"
+						placeholder="enter task"
+						value={input}
+						onChange= {(e) => {setInput(e.target.value)}}
+					/>
+					<button type="button" class="btn btn-primary" onClick={() => setList([...list], input)}>
+						Submit
+					</button>
+				</div>
+			</div>
+			{list.map((e,i)=>{
+				<div key={i} className="task">
+					{e}
+				</div>
+			})}
 		</div>
 	);
 };
